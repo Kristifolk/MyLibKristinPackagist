ebf3a1bb79a9eb655e4a95c225529ebfef0024a5
kristin <devkristin.karr@gmail.com>
2024-01-26 12:59:20 +0300

    refactoring code

--- src/ExchangedAmount.php
+++ src/ExchangedAmount.php
@@ -4,10 +4,11 @@ namespace MyLib;
 
 class ExchangedAmount
 {
-    private $from;
-    private $to;
-    private $amount;
-    private $xml;
+    /*
+     конструктор до версии php 8.0
+    private string $from;
+    private string $to;
+    private int $amount;
 
     public function __construct($from, $to, $amount)
     {
@@ -15,36 +16,53 @@ class ExchangedAmount
         $this->to = $to;
         $this->amount = $amount;
     }
+    */
+
+    //конструктор в версии php 8
+    public function __construct(
+        private readonly string $from,
+        private readonly string $to,
+        private readonly int $amount
+    ) {
+    }
 
     private function dataAPI(): \SimpleXMLElement
     {
         $date = date('d/m/Y'); // Текущая дата
-        $url = 'https://www.cbr.ru/scripts/XML_daily.asp?date_req=' . $date;//котировки на текущий день ЦБР
+        $url = 'https://www.cbr.ru/scripts/XML_daily.asp?date_req=' . $date;  // котировки на текущий день ЦБР
 
         $xml = simpleXML_load_file($url, "SimpleXMLElement", LIBXML_NOCDATA);
-        //simplexml_load_file — Интерпретирует XML-файл в объект. принимает три параметра: URL-адрес файла, тип объекта, который будет создан из XML-данных (в данном случае SimpleXMLElement), и флаг LIBXML_NOCDATA, который указывает, что CDATA-секции в XML-файле не должны быть преобразованы в объекты.
+
+        if (!$xml) {
+            var_dump("Ошибка подключения!");
+            die();
+        }
+
         return $xml;
+
+        //simplexml_load_file — Интерпретирует XML-файл в объект. принимает три параметра: URL-адрес файла, тип объекта,
+        // который будет создан из XML-данных (в данном случае SimpleXMLElement), и флаг LIBXML_NOCDATA,
+        // который указывает, что CDATA-секции в XML-файле не должны быть преобразованы в объекты.
     }
 
-    private function value($currencyLetterCode): float
+    private function value(string $currencyLetterCode): float
     {
-        foreach ($this->xml as $element) {
+        foreach ($this->dataAPI() as $element) {
             $charCode = $element->CharCode;
             if ($currencyLetterCode == $charCode) {
-                $value = floatval(str_replace(',', '.', $element->Value)); // Заменяем запятую на точку
-                return $value;
+                // Заменяем запятую на точку
+                return floatval(str_replace(',', '.', $element->Value));
             }
         }
         return 0;
     }
 
-    private function nominal($currencyLetterCode): float
+    private function nominal(string $currencyLetterCode): float
     {
-        foreach ($this->xml as $element) {
+        foreach ($this->dataAPI() as $element) {
             $charCode = $element->CharCode;
             if ($currencyLetterCode == $charCode) {
-                $nominal = floatval($element->Nominal);
-                return $nominal;
+                return floatval($element->Nominal);
             }
         }
         return 0;
@@ -52,14 +70,11 @@ class ExchangedAmount
 
     public function toDecimal(): float
     {
-        $this->xml = $this->dataAPI();
         $valueFrom = $this->value($this->from);
         $nominalFrom = $this->nominal($this->from);
         $valueTo = $this->value($this->to);
         $nominalTo = $this->nominal($this->to);
-        $amountTo = round($this->amount * (($valueFrom / $nominalFrom) / ($valueTo / $nominalTo)), 2);
-        //print_r($amountTo);
-        return $amountTo;
+        return round($this->amount * (($valueFrom / $nominalFrom) / ($valueTo / $nominalTo)), 2);
     }
 }
 
